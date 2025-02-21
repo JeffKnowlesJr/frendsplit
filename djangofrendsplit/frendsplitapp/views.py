30a7ab1574464bd735947c7def631eb0ccac2d36
jeffknowlesjr <jeffknowlesjr@gmail.com>
2025-02-21 01:56:02 -0500

    feat: Add user signup functionality
    - Implement signup view with UserCreationForm
    - Add signup URL route
    - Configure authentication settings and template directories
    - Enable user registration and automatic login after signup

--- djangofrendsplit/frendsplitapp/views.py
+++ djangofrendsplit/frendsplitapp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login
 
 def index(request):
     return render(request, 'frendsplitapp/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully!')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'frendsplitapp/signup.html', {'form': form})
--- djangofrendsplit/mysite/settings.py
+++ djangofrendsplit/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
