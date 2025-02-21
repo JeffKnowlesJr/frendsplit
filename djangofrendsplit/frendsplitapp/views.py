fa84cc32f7e6bcfc04cda87e13dd237d6431185d
jeffknowlesjr <jeffknowlesjr@gmail.com>
2025-02-21 01:36:18 -0500

    feat: Update index view to render template
    - Replace hardcoded HttpResponse with template rendering
    - Remove unnecessary imports
    - Prepare index view for dynamic content display

--- djangofrendsplit/frendsplitapp/views.py
+++ djangofrendsplit/frendsplitapp/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import login
 
-
 def index(request):
-    return HttpResponse("Hello, world. Welcome to the FrendSplit app.")
+    return render(request, 'frendsplitapp/index.html')
 
 def signup(request):
     if request.method == 'POST':
